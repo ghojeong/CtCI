b666f6fa7a5f4c52586cec6c015f9259364ce005
고정완 (Can) <jw.gho@ecubelabs.com>
2020-03-08 10:21:04 +0900

    Revert "Q1_02_Check_Permutation"
    This reverts commit 9cfee6656e96b76466f477cf94f40262e7f81811.

--- Java/Ch 01. Arrays and Strings/Q1_02_Check_Permutation/QuestionB.java
+++ Java/Ch 01. Arrays and Strings/Q1_02_Check_Permutation/QuestionB.java
@@ -1,58 +1,26 @@
 package Q1_02_Check_Permutation;
 
-public class QuestionB {
+public class QuestionB {	
 	public static boolean permutation(String s, String t) {
-		if (s.length() != t.length()) {
-			return false;
-		}
-
-		int min_char_s = 1000000;
-		int min_char_t = 1000000;
-		int max_char_s = 0;
-		int max_char_t = 0;
-
+		if (s.length() != t.length()) return false; // Permutations must be same length
+		
+		int[] letters = new int[128]; // Assumption: ASCII
 		for (int i = 0; i < s.length(); i++) {
-			if (s.charAt(i) > max_char_s) {
-				max_char_s = s.charAt(i);
-			}
-			if (s.charAt(i) < min_char_s) {
-				min_char_s = s.charAt(i);
-			}
-			if (t.charAt(i) > max_char_t) {
-				max_char_t = t.charAt(i);
-			}
-			if (t.charAt(i) < min_char_t) {
-				min_char_t = t.charAt(i);
-			}
+			letters[s.charAt(i)]++;
 		}
-
-		if (max_char_s != max_char_t) {
-			return false;
+		  
+		for (int i = 0; i < t.length(); i++) {
+			letters[t.charAt(i)]--;
+		    if (letters[t.charAt(i)] < 0) {
+		    	return false;
+		    }
 		}
-		if (min_char_s != min_char_t) {
-			return false;
-		}
-
-		int offset = min_char_s;
-		int counter_length = max_char_s - min_char_s + 1;
-
-		int[] s_counter = new int[counter_length];
-		int[] t_counter = new int[counter_length];
-		for (int i = 0; i < s.length(); i++) {
-			s_counter[s.charAt(i) - offset]++;
-			t_counter[t.charAt(i) - offset]++;
-		}
-
-		for (int i = 0; i < counter_length; i++) {
-			if (s_counter[i] != t_counter[i]) {
-				return false;
-			}
-		}
-		return true;
+		  
+		return true; // letters array has no negative values, and therefore no positive values either
 	}
-
+	
 	public static void main(String[] args) {
-		String[][] pairs = { { "apple", "papel" }, { "carrot", "tarroc" }, { "hello", "llloh" } };
+		String[][] pairs = {{"apple", "papel"}, {"carrot", "tarroc"}, {"hello", "llloh"}};
 		for (String[] pair : pairs) {
 			String word1 = pair[0];
 			String word2 = pair[1];
