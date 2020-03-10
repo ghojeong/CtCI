c0a894f8adbf7a1747a1a2d1eae35e3ab54018f7
고정완 (Can) <jw.gho@ecubelabs.com>
2020-03-10 15:52:15 +0900

    불필요한 printLinkedList 삭제

--- Java/Ch 02. Linked Lists/Q2_01_Remove_Dups/QuestionGho.java
+++ Java/Ch 02. Linked Lists/Q2_01_Remove_Dups/QuestionGho.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import java.util.Iterator;
 
 public class QuestionGho {
-  public static LinkedList<Integer> deleteDups(LinkedList<Integer> ll) {
+  private static LinkedList<Integer> deleteDups(LinkedList<Integer> ll) {
     LinkedList<Integer> deletedLl = new LinkedList<Integer>();
     HashSet<Integer> set = new HashSet<Integer>();
     Iterator<Integer> itr = ll.iterator();
@@ -19,24 +19,14 @@ public class QuestionGho {
     return deletedLl;
   }
 
-  public static void printLinkedList(LinkedList<Integer> ll) {
-    Iterator<Integer> itr = ll.iterator();
-    while (itr.hasNext()) {
-      System.out.print(itr.next());
-      if (itr.hasNext())
-        System.out.print(" -> ");
-    }
-  }
-
   public static void main(String[] args) {
 
     LinkedList<Integer> ll = new LinkedList<Integer>();
     for (int i = 1; i < 100; i++) {
       ll.add(i % 10);
     }
-    printLinkedList(ll);
+    System.out.println(ll.toString());
     System.out.println();
-    System.out.println();
-    printLinkedList(deleteDups(ll));
+    System.out.print(deleteDups(ll).toString());
   }
 }
