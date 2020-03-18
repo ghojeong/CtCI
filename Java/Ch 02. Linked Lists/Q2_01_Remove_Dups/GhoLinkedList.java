67ebd42257f014844384073957b82c841e240650
고정완 (Can) <jw.gho@ecubelabs.com>
2020-03-18 11:50:25 +0900

    Q2_01_Remove_Dups 다시 풀음

---
+++ Java/Ch 02. Linked Lists/Q2_01_Remove_Dups/GhoLinkedList.java
@@ -0,0 +1,68 @@
+package Q2_01_Remove_Dups;
+
+import java.util.HashSet;
+
+public class GhoLinkedList<T> {
+  private class Node<T> {
+    private T value;
+    private Node<T> next;
+
+    Node(T value) {
+      this.value = value;
+    };
+  }
+
+  public Node<T> head;
+  public Node<T> tail;
+
+  public GhoLinkedList() {
+    this.head = null;
+    this.tail = null;
+  };
+
+  public void add(T value) {
+    Node<T> node = new Node(value);
+    if (head == null) {
+      head = node;
+      tail = node;
+    } else {
+      tail.next = node;
+      tail = tail.next;
+    }
+  }
+
+  public String toString() {
+    if (head == null) {
+      return "";
+    }
+    Node<T> itr = head;
+    String str = "\n[" + itr.value;
+    itr = itr.next;
+    while (itr != null) {
+      str += " -> " + itr.value;
+      itr = itr.next;
+    }
+    return str + "]";
+  }
+
+  public void deleteDups() {
+    if (head == null) {
+      return;
+    }
+    HashSet<T> set = new HashSet<T>();
+    Node<T> itr = head;
+    Node<T> itrPrev = itr;
+    itr = itr.next;
+    while (itr != null) {
+      if (set.contains(itr.value)) {
+        // remove current itr Node
+        itrPrev.next = itr.next;
+        itr = itrPrev.next;
+      } else {
+        set.add(itr.value);
+        itrPrev = itr;
+        itr = itr.next;
+      }
+    }
+  }
+}
--- Java/Ch 02. Linked Lists/Q2_01_Remove_Dups/QuestionGho.java
+++ Java/Ch 02. Linked Lists/Q2_01_Remove_Dups/QuestionGho.java
@@ -1,33 +1,17 @@
 package Q2_01_Remove_Dups;
 
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Iterator;
 import java.util.Random;
 
 public class QuestionGho {
-  private static <T> LinkedList<T> deleteDups(LinkedList<T> ll) {
-    HashSet<T> set = new HashSet<T>();
-    Iterator<T> itr = ll.iterator();
-    while (itr.hasNext()) { // O(n)
-      T val = itr.next();
-      if (set.contains(val)) { // O(1)
-        itr.remove(); // O(1)
-      } else {
-        set.add(val);
-      }
-    }
-    return ll;
-  }
 
   public static void main(String[] args) {
-    LinkedList<Character> ll = new LinkedList<Character>();
+    GhoLinkedList<Character> ll = new GhoLinkedList<Character>();
     Random random = new Random();
     for (int i = 1; i < 100; i++) {
       ll.add((char) (random.nextInt(20) + 63));
     }
-    System.out.println(ll.toString());
-    System.out.println();
-    System.out.print(deleteDups(ll).toString());
+    System.out.println(ll);
+    ll.deleteDups();
+    System.out.print(ll);
   }
 }
