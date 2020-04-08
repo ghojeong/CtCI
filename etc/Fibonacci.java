d75614c6ddc9c13c50649bdf79afbc04c5ae0c87
고정완 (Can) <jw.gho@ecubelabs.com>
2020-04-08 16:29:28 +0900

    피보나치 수열 풀이

---
+++ etc/Fibonacci.java
@@ -0,0 +1,71 @@
+public class Fibonacci {
+
+  // Temporal Complexity: O(n^2)
+  // Spatial Complexity: O(n^2)
+  public static int fibRec(int n) {
+    if (n < 1) {
+      return 0;
+    }
+    if (n == 1) {
+      return 1;
+    }
+    return fibRec(n - 1) + fibRec(n - 2);
+  }
+
+  // Temporal Complexity: O(n)
+  // Spatial Complexity: O(n)
+  public static int fibDynamic(int n) {
+    if (n < 1) {
+      return 0;
+    }
+    if (n < 3) {
+      return 1;
+    }
+    int f[] = new int[n + 1];
+    int i;
+
+    f[0] = 0;
+    f[1] = 1;
+    for (i = 2; i <= n; i++) {
+      f[i] = f[i - 1] + f[i - 2];
+    }
+    return f[n];
+  }
+
+  // Temporal Complexity: O(n)
+  // Spatial Complexity: O(1)
+  public static int fibDynamicOptimized(int n) {
+    if (n < 1) {
+      return 0;
+    }
+    if (n < 3) {
+      return 1;
+    }
+    int f_2 = 0, f_1 = 1, f = 1;
+    for (int i = 2; i <= n; i++) {
+      f = f_1 + f_2;
+      f_2 = f_1;
+      f_1 = f;
+    }
+    return f;
+  }
+
+  public static void main(String a[]) {
+    int n = 25;
+
+    System.out.println("\n Recursion");
+    for (int i = 0; i < n; i++) {
+      System.out.print(fibRec(i) + " ");
+    }
+
+    System.out.println("\n\n Dynamic Programming");
+    for (int i = 0; i < n; i++) {
+      System.out.print(fibRec(i) + " ");
+    }
+
+    System.out.println("\n\n Optimized Dynamic Programming");
+    for (int i = 0; i < n; i++) {
+      System.out.print(fibRec(i) + " ");
+    }
+  }
+}
