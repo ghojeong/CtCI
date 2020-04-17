6539a9d8faa737d28c08868be8668d6570cbeb1b
고정완 (Can) <jw.gho@ecubelabs.com>
2020-04-17 10:40:54 +0900

    Quick sort 간략화

--- sort/Quick.java
+++ sort/Quick.java
@@ -2,47 +2,27 @@ package sort;
 
 public class Quick {
 
-  /*
-   * This function takes last element as pivot, places the pivot element at its
-   * correct position in sorted array, and places all smaller (smaller than pivot)
-   * to left of pivot and all greater elements to right of pivot
-   */
-  private static int partition(int arr[], int low, int high) {
-    int pivot = arr[high];
-    int i = (low - 1); // index of smaller element
-    for (int j = low; j < high; j++) {
-      // If current element is smaller than the pivot
-      if (arr[j] < pivot) {
-        i++;
-
-        // swap arr[i] and arr[j]
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-      }
-    }
-
-    // swap arr[i+1] and arr[high] (or pivot)
-    int temp = arr[i + 1];
-    arr[i + 1] = arr[high];
-    arr[high] = temp;
-
-    return i + 1;
+  public static void swap(int arr[], int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
   }
 
-  /*
-   * The main function that implements QuickSort() arr[] --> Array to be sorted,
-   * low --> Starting index, high --> Ending index
-   */
+  private static int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int pi = low; // partitioning index
+    for (int i = low; i < high; i++) {
+      if (arr[i] < pivot) {
+        swap(arr, pi++, i);
+      }
+    }
+    swap(arr, pi, high);
+    return pi;
+  }
+
   private static void sort(int arr[], int low, int high) {
     if (low < high) {
-      /*
-       * pi is partitioning index, arr[pi] is now at right place
-       */
-      int pi = partition(arr, low, high);
-
-      // Recursively sort elements before
-      // partition and after partition
+      int pi = partition(arr, low, high); // partitioning index
       sort(arr, low, pi - 1);
       sort(arr, pi + 1, high);
     }
@@ -50,9 +30,7 @@ public class Quick {
 
   public static int[] sorted(int[] array) {
     int[] arr = array.clone();
-
     sort(arr, 0, arr.length - 1);
-
     return arr;
   }
 }
