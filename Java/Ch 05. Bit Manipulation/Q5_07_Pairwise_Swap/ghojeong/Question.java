f79ef2ec27c4c0115867e74c261560d52b2447fe
고정완 (Can) <jw.gho@ecubelabs.com>
2020-05-19 12:29:42 +0900

    Q5_07_Pairwise_Swap 풀이 추가

--- Java/Ch 05. Bit Manipulation/Q5_07_Pairwise_Swap/ghojeong/Question.java
+++ Java/Ch 05. Bit Manipulation/Q5_07_Pairwise_Swap/ghojeong/Question.java
@@ -12,7 +12,20 @@ public class Question {
 	}
 
 	public static int swapOddEvenBits(int x) {
-		return (((x & 0xaaaaaaaa) >>> 1) | ((x & 0x55555555) << 1));
+		int ret = 0;
+		int odd = 1;
+		int even = 2;
+		for (int i = 0; i < 16; i++) {
+			ret |= (odd & x) << 1;
+			ret |= (even & x) >> 1;
+			// ret |= ((odd & x) << 1) | ((even & x) >> 1);
+			odd <<= 2;
+			even <<= 2;
+		}
+		return ret;
+
+		// 0101 = 1 + 4 = 5, 1010 = 2 + 8 = 10 = a
+		// return (((x & 0xaaaaaaaa) >>> 1) | ((x & 0x55555555) << 1));
 	}
 
 	public static void main(String[] args) {
