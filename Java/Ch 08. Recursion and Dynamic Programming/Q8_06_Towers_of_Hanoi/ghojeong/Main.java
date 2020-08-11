2d11e73639013434e7141d5e51437579753ae422
고정완 (Can) <jw.gho@ecubelabs.com>
2020-08-11 16:17:51 +0900

    Enhance Q8_06_Towers_of_Hanoi

--- Java/Ch 08. Recursion and Dynamic Programming/Q8_06_Towers_of_Hanoi/ghojeong/Main.java
+++ Java/Ch 08. Recursion and Dynamic Programming/Q8_06_Towers_of_Hanoi/ghojeong/Main.java
@@ -3,11 +3,9 @@ package Q8_06_Towers_of_Hanoi.ghojeong;
 import java.io.*;
 
 public class Main {
-  public static BufferedWriter bw;
   public static BufferedReader br;
-  public static StringBuilder sb;
-  public static int cnt = 0;
-  public static int numberOfDisks;
+  public static BufferedWriter bw;
+  public static int n;
 
   public static class Tower {
     private int[] diskStack;
@@ -15,51 +13,46 @@ public class Main {
     private final int name;
 
     public Tower(int name) {
-      this.diskStack = new int[numberOfDisks];
+      this.diskStack = new int[n];
       this.name = name;
     }
 
     private void moveTopTo(Tower t) {
-      if (t.diskStack[t.top] > this.diskStack[this.top] && this.top > 0 && t.top < numberOfDisks - 1) {
+      if (t.top < n - 1 && this.top > 0 && t.diskStack[t.top] > this.diskStack[this.top]) {
         t.diskStack[++t.top] = this.diskStack[this.top--];
       }
     }
 
-    public void moveDisks(int quantity, Tower destination, Tower buffer) {
+    public void moveDisks(int quantity, Tower destination, Tower buffer) throws IOException {
       if (quantity <= 0) {
         return;
       }
-
       moveDisks(quantity - 1, buffer, destination);
-      sb.append('\n');
-      sb.append(this.name);
-      sb.append(' ');
-      sb.append(destination.name);
-      cnt++;
+      bw.write("\n" + this.name + " " + destination.name);
+      bw.flush();
       moveTopTo(destination);
       buffer.moveDisks(quantity - 1, destination, this);
     }
   }
 
   public static void main(String[] args) throws IOException {
-    bw = new BufferedWriter(new OutputStreamWriter(System.out));
     br = new BufferedReader(new InputStreamReader(System.in));
-    sb = new StringBuilder();
-    numberOfDisks = Integer.parseInt(br.readLine());
+    n = Integer.parseInt(br.readLine());
+    br.close();
+    bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     Tower source = new Tower(1);
     Tower buffer = new Tower(2);
     Tower destination = new Tower(3);
 
-    for (int i = 0; i < numberOfDisks; i++) {
-      source.diskStack[i] = numberOfDisks - i;
+    for (int i = 0; i < n; i++) {
+      source.diskStack[i] = n - i;
     }
-    source.top = numberOfDisks - 1;
+    source.top = n - 1;
 
-    source.moveDisks(numberOfDisks, destination, buffer);
+    bw.write(Integer.toString((1 << n) - 1));
+    source.moveDisks(n, destination, buffer);
 
-    bw.write(cnt + sb.toString() + "\n");
-    br.close();
     bw.flush();
     bw.close();
   }
